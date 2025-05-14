40a4750fad20dc212e23d807e24e1092d1ab92ce
Bekie <dret8587@naver.com>
2025-05-14 22:05:02 +0900

    25-05-14 문자열,리스트,사전 학습

---
+++ 4_practice/04-05 탈출 문자.py
@@ -0,0 +1,20 @@
+#탈출 문자
+print("백백문이 불여일견\n백견이 불여일타") # \n : 줄바꿈
+
+ # 저는 "나도 코딩입니다"
+#print("저는 "나도코딩"입니다.") # ""가 문자열로 처리되며 에러러
+print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다.") # \"\' : 문장 내에서 따옴표
+
+# \\ : 문장 내에서 
+#pinrt("C:\Users\dret8\OneDrive\Desktop\PythonWorkspace>")"
+print("C:\\Users\\dret8\\OneDrive\\Desktop\\PythonWorkspace>")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine") # 맨 앞에서 Pine을 넣어 PineApple 출력
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
